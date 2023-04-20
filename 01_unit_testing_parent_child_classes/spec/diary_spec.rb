0772c05b725d79cefbbd657a456668184cd3917f
Verity <veritywong90@gmail.com>
2023-04-20 19:08:23 +0100

    using doubles

--- 01_unit_testing_parent_child_classes/spec/diary_spec.rb
+++ 01_unit_testing_parent_child_classes/spec/diary_spec.rb
@@ -3,32 +3,17 @@ require 'diary'
 RSpec.describe Diary do
     it "adds and lists diary entries" do
         diary = Diary.new
-        fake_diary_entry = FakeDiaryEntry.new
+        fake_diary_entry = double :diary_entry
         diary.add(fake_diary_entry)
         expect(diary.entries).to eq [fake_diary_entry]
     end
 
     it "counts words in entries given" do
         diary = Diary.new
-        fake_diary_entry_1 = FakeTwoWordDiaryEntry.new
-        fake_diary_entry_2 = FakeThreeWordDiaryEntry.new
+        fake_diary_entry_1 = double :diary_entry, count_words: 2
+        fake_diary_entry_2 = double :diary_entry, count_words: 3
         diary.add(fake_diary_entry_1)
         diary.add(fake_diary_entry_2)
         expect(diary.count_words).to eq 5
-    end
-end
-
-class FakeDiaryEntry
-end
-
-class FakeTwoWordDiaryEntry
-    def count_words
-        2
-    end
-end
-
-class FakeThreeWordDiaryEntry
-    def count_words
-        3
     end
 end
